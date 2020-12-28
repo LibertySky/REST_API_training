92d8f3e233a83411c6bf204d2c6014223832e855
LibertySky <hello@LibertySkygraphics.com>
2020-12-28 09:56:18 -0400

    DB connection

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
@@ -18,22 +19,33 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res.status(422).json({
-			message: 'Error: validation failed, entered data is incorrect',
-			errors: errors.array(),
-		});
+		const error = new Error(
+			'Error: validation failed, entered data is incorrect'
+		);
+		error.statusCode = 422;
+		throw error;
 	}
 	const title = req.body.title;
 	const content = req.body.content;
-	res.status(201).json({
-		message: 'Post created successfully',
-		post: {
-			_id: new Date().toISOString(),
-			title: title,
-			content: content,
-			imageUrl: 'images/duck.jpg',
-			creator: { name: 'LibertySky' },
-			createdAt: new Date(),
-		},
+	const post = new Post({
+		title: title,
+		content: content,
+		imageUrl: 'images/duck.jpg',
+		creator: { name: 'LibertySky' },
 	});
+	post
+		.save()
+		.then((result) => {
+			console.log(result);
+			res.status(201).json({
+				message: 'Post created successfully',
+				post: result,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // routes
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // set headers to any response to avoir CORS errors
 app.use((req, res, next) => {
@@ -19,5 +22,20 @@ app.use((req, res, next) => {
 });
 app.use('/feed', feedRoutes);
 
+// error handling middleware
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	res.status(status).json({ message: message });
+});
+
 const PORT = process.env.PORT || 3021;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+mongoose
+	.connect('mongodb://localhost:27017/RestAPI-training', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+	});
