9bfdc226bd98432056cd08f835489a1af8c3e4dc
LibertySky <hello@LibertySkygraphics.com>
2020-12-31 08:55:28 -0400

    Auth with JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
 	const errors = validationResult(req);
@@ -27,6 +29,48 @@ exports.signup = (req, res, next) => {
 		})
 		.then((result) => {
 			res.status(201).json({ message: 'User created!', userId: result._id });
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+exports.login = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	let loadedUser;
+	User.findOne({ email: email })
+		.then((user) => {
+			if (!user) {
+				const error = new Error(
+					`Error: A user with email: ${email} could not be found`
+				);
+				error.statusCode = 401;
+				error.data = error.errors.array();
+				throw error;
+			}
+			loadedUser = user;
+			return bcrypt.compare(password, user.password);
+		})
+		.then((isEqual) => {
+			if (!isEqual) {
+				const error = new Error(`Error: Wrong password`);
+				error.statusCode = 401;
+				error.data = error.errors.array();
+				throw error;
+			}
+			const token = jwt.sign(
+				{
+					email: loadedUser.email,
+					userId: loadedUser._id.toString(),
+				},
+				process.env.JWT_PRIVATE_KEY,
+				{ expiresIn: '1h' }
+			);
+			res.status(200).json({ token: token, userId: loadedUser._id.toString() });
 		})
 		.catch((err) => {
 			if (!err.statusCode) {
